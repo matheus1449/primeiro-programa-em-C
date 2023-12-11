edf751ac6638b14c5811c9f2f8cbb560618498ad
Matheus Francisco <matheus.1449@outlook.com>
2023-12-11 15:53:51 -0300

    criei meu primeiro pequeno projeto

---
+++ primeiro_if.c
@@ -0,0 +1,13 @@
+int main(){
+       int a;
+       int senha;
+       senha=123;
+       a=12;
+       printf("Mostra o valor da variavel a....%d\n\n\n", a);
+       printf("digite sua senha....");
+       if(senha==1234)
+       printf("senha valida");
+       else
+       printf("senha invalida");
+       printf("\n\n\n");
+       }
